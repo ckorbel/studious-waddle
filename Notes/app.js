247eeb69e7ac0dc39b524ab629e8382b1e38f567
Chris Korbel <ckorbel@ckorbel-lap.local>
2018-07-27 09:26:20 -0700

    restructure

--- Notes/app.js
+++ Notes/app.js
@@ -2,10 +2,17 @@ console.log('starting app');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-let res = notes.add(2, 4);
-console.log(res);
+
+console.log(_.isString(true));
+console.log(_.isString('Chris'));
+console.log(_.uniq([1,'Chris',2,3,1,4,5,'Chris',11]));
+
+
+// let res = notes.add(2, 4);
+// console.log(res);
 
 // let user = os.userInfo();
 
