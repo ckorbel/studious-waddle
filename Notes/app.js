c1927d4b508db585d68e2666c37291e81b302eff
Chris Korbel <ckorbel1@gmail.com>
2018-08-04 14:10:43 -0700

    notes

--- Notes/app.js
+++ Notes/app.js
@@ -5,7 +5,31 @@ const yargs = require('yargs');
 //created module
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const titleOptions = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+};
+
+const bodyOptions = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+};
+
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    }).command('list', 'list all notes')
+    .command('read', 'Read a note', {
+        title: titleOptions
+    })
+    .command('remove', 'remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
 var command = argv._[0];
 console.log('Command: ', command);
 console.log('Yargs', argv);
