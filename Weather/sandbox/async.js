c1dd8737bb6da0acad84cf5581b74989b2fb9caa
Chris Korbel <ckorbel1@gmail.com>
2018-08-05 16:23:50 -0700

    test

--- Weather/sandbox/async.js
+++ Weather/sandbox/async.js
@@ -1,11 +1,27 @@
-console.log('starting app');
+// console.log('starting app');
 
-setTimeout(() => {
-    console.log('its firing');
-}, 2000);
+// setTimeout(() => {
+//     console.log('its firing');
+// }, 2000);
 
-setTimeout(() => {
-    console.log('2nd one');
-}, 0);
+// setTimeout(() => {
+//     console.log('2nd one');
+// }, 0);
 
-console.log('finishing up');
+// console.log('finishing up');
+
+//Callbacks.js
+
+var getUser = (id, callback) => {
+    var user = {
+        id: id,
+        name: 'Chris'
+    };
+    setTimeout(() => {
+        callback(user);
+    }, 3000);
+};
+
+getUser(1, (user) => {
+    console.log(user);
+});
