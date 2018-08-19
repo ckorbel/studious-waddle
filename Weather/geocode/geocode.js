786b275ce20dccfb6ae4e3b7aaa176df13792bf3
Chris Korbel <ckorbel1@gmail.com>
2018-08-18 17:07:16 -0700

    promise

--- Weather/geocode/geocode.js
+++ Weather/geocode/geocode.js
@@ -9,6 +9,7 @@ var geocodeAddress = (address, callback) => {
     }, (error, response, body) => {
         if (error) {
             callback(`Unable to connect to Google servers`); //(errorMessage, )
+            //callback();
         } else if (body.status === 'ZERO_RESULTS') {
             callback('Unable to find address.');
         } else if (body.status === 'OK') {
--- Weather/sandbox/promise.js
+++ Weather/sandbox/promise.js
@@ -1,3 +1,15 @@
 var somePromise = new Promise((resolve, reject) => {
-    resolve
+    setTimeout(() => {
+        resolve('Hey. It worked');
+        resolve();
+        reject('Unable to fufill promise');
+    }, 2500);
 });
+
+somePromise.then((message) => {
+    console.log('Succees: ', message);
+}, (errorMessage) => {
+    console.log('Error: ', errorMessage);
+});
+
+//once you set a promises state to either fufilled or rejected you can't set it again
