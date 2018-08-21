17be0b5f50777b1db36e314f9a55669380f1e61d
Chris Korbel <ckorbel1@gmail.com>
2018-08-20 21:11:42 -0700

    testing promises

--- Weather/sandbox/promise.js
+++ Weather/sandbox/promise.js
@@ -1,15 +1,40 @@
-var somePromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve('Hey. It worked');
-        resolve();
-        reject('Unable to fufill promise');
-    }, 2500);
-});
+//promise chaining synchonous two things 
 
-somePromise.then((message) => {
-    console.log('Succees: ', message);
-}, (errorMessage) => {
-    console.log('Error: ', errorMessage);
-});
+var asyncAdd = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (typeof a === 'number' && typeof b === 'number') {
+                resolve(a + b);
+            } else {
+                reject('Arguments must be numbers');
+            }
+        }, 1500);
+    });
+};
+
+asyncAdd(5, 17).then((res) => {
+    console.log('Result: ', res);
+    return asyncAdd(res, 33)
+}, (err) => {
+    console.log(err);
+}).then((res, err) => {
+    console.log(`Should be 45`);
+}, () => {
+
+})
+
+// var somePromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('Hey. It worked');
+//         resolve();
+//         reject('Unable to fufill promise');
+//     }, 2500);
+// });
+
+// somePromise.then((message) => {
+//     console.log('Succees: ', message);
+// }, (errorMessage) => {
+//     console.log('Error: ', errorMessage);
+// });
 
 //once you set a promises state to either fufilled or rejected you can't set it again
