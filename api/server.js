fbcbda58c8a14b13da5d15d5507a93781b092bc1
Chris Korbel <ckorbel1@gmail.com>
2018-09-06 12:11:45 -0700

    added server.log

--- api/server.js
+++ api/server.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const fs = require('fs');
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+
+//middleware
 app.use(express.static(__dirname + '/public'));
 
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+//end middleware
+
+//Handlebar helpers
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
 });
@@ -14,8 +36,9 @@ hbs.registerHelper('getCurrentYear', () => {
 hbs.registerHelper('screamIt', (text) => {
     return text.toUpperCase();
 });
+//end handlerbar helpers
 
-
+//Routes 
 app.get('/', (req, res) => {
     res.render('home.hbs', {
         pageTitle: 'Home Page',
@@ -29,11 +52,20 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects',
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         error: 'Unable to fufill request'
     });
 });
+
+
+//end of route
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
