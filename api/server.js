4e2637430fe380d8494046ab88d10a119110ee45
Chris Korbel <ckorbel1@gmail.com>
2018-09-06 10:21:17 -0700

    integrated handlebars added some partials

--- api/server.js
+++ api/server.js
@@ -1,19 +1,32 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+
 app.get('/', (req, res) => {
-    //res.send('<h1>Hello Express</h1>');
-    res.send({
-        name: 'Chris',
-        likes: ['warriors', '49ers', 'Giants']
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to this website',
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('About Page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+    });
 });
 
 app.get('/bad', (req, res) => {
