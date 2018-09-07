fa8940e104f288c5ad2b1ce8ff5bcb2156f14eaa
Chris Korbel <ckorbel1@gmail.com>
2018-09-07 09:55:06 -0700

    async testing

--- mocha-tests/utils/utils.test.js
+++ mocha-tests/utils/utils.test.js
@@ -14,6 +14,23 @@ it('should return square of number', () => {
 });
 
 
+//async test
+it('should async add two numbers', (done) => {
+    utils.asyncAdd(4, 3, (sum) => {
+        expect(sum).toBe(7).toBeA('number');
+        done();
+    });
+});
+
+it('should async return square of number', (done) => {
+    utils.asyncSquare(4, (res) => {
+        expect(res).toBe(16).toBeA('number');
+        done();
+    });
+});
+//mocha warns you when tests take a long time setTimeout
+
+//object test
 it('should set firstName and lastName', () => {
     var user = {
         location: 'San Jose',
@@ -24,13 +41,15 @@ it('should set firstName and lastName', () => {
         firstName: 'Chris',
         lastName: 'Korbel'
     });
-
     // expect(user).toEqual(results); //objects update by reference in JS so this should pass
 });
 
+//toBe would check if they are exact same === and they are two different objects so test will fail instead must use .toEqual() or .toNotEqual for objects and arrays
+
+
 // it('should expext some values', () => {
 //     // expect(12).toNotBe(11);
-//     // expect({ name: 'Chris' }).toNotEqual({ name: 'chris' }); //toBe would check if they are exact same === and they are two different objects so test will fail instead must use .toEqual() or .toNotEqual for objects and arrays
+//     // expect({ name: 'Chris' }).toNotEqual({ name: 'chris' }); 
 //     //expect([2, 3, 4]).toExclude(1);
 //     expect({
 //         name: 'Chris',
